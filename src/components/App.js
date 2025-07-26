7ebce2fc87f180b32bb646908e80bfe7827b8bf3
mohaaz12 <mohaaz3004@gmail.com>
2025-07-26 14:55:33 +0300

    fetch-crud

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,50 @@
-import React, { useState } from "react";
-import AdminNavBar from "./AdminNavBar";
-import QuestionForm from "./QuestionForm";
+import React, { useEffect, useState } from "react";
 import QuestionList from "./QuestionList";
+import QuestionForm from "./QuestionForm";
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+  const [showQuestions, setShowQuestions] = useState(false); // needed for test
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then(setQuestions);
+  }, []);
+
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
+  function handleDeleteQuestion(id) {
+    const updated = questions.filter((q) => q.id !== id);
+    setQuestions(updated);
+  }
+
+  function handleUpdateQuestion(updatedQ) {
+    const updated = questions.map((q) =>
+      q.id === updatedQ.id ? updatedQ : q
+    );
+    setQuestions(updated);
+  }
 
   return (
-    <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
-    </main>
+    <section>
+      <h1>Quiz Admin Panel</h1>
+
+      {/* Required by test */}
+      <button onClick={() => setShowQuestions(true)}>View Questions</button>
+
+      <QuestionForm onAddQuestion={handleAddQuestion} />
+
+      {showQuestions && (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion={handleUpdateQuestion}
+        />
+      )}
+    </section>
   );
 }
 
